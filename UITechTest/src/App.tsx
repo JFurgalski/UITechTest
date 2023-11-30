a22b7f9f5b9466d022269282e2c8f8e194e5c0c2
Yaz Furgalski <jaroslaw.furgalski@spotlightsportsgroup.com>
2023-11-30 20:20:53 +0000

    Delete all mocks

--- UITechTest/src/App.tsx
+++ UITechTest/src/App.tsx
@@ -1,47 +1,11 @@
-import { useRef } from "react";
 import MovieComponent from "./Components/MovieComponent/MovieComponent";
 import Title from "./Components/Title/Title";
 
-// TODO: use https://giddy-beret-cod.cyclic.app/movieCompanies
-const mockMovieCompanyData: any = [{ id: "1", name: "Test Productions" }];
-
-// TODO: use https://giddy-beret-cod.cyclic.app/movies
-const mockMovieData: any = [
-  {
-    id: "1",
-    reviews: [6, 8, 3, 9, 8, 7, 8],
-    title: "A Testing Film",
-    filmCompanyId: "1",
-    cost: 534,
-    releaseYear: 2005,
-  },
-  {
-    id: "2",
-    reviews: [5, 7, 3, 4, 1, 6, 3],
-    title: "Mock Test Film",
-    filmCompanyId: "1",
-    cost: 6234,
-    releaseYear: 2006,
-  },
-];
-
 export const App = () => {
-
-  const refreshButton = (buttonText: any) => {
-    if (mockMovieCompanyData) {
-      return <button>{buttonText}</button>;
-    } else {
-      return <p>No movies loaded yet</p>;
-    }
-  };
-
   return (
     <div>
       <Title title="Welcome to Movie database!" />
-      {refreshButton("Refresh")}
-      <div>
-        <MovieComponent />
-      </div>
+      <MovieComponent />
     </div>
   );
 };
