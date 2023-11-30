02f8e6e2dc6c383950db27e983c667110118c1e2
Yaz Furgalski <jaroslaw.furgalski@spotlightsportsgroup.com>
2023-11-30 19:00:16 +0000

    Custom button component && types

---
+++ UITechTest/src/Components/Button/CustomButton.tsx
@@ -0,0 +1,18 @@
+import * as React from "react";
+import Button from "@mui/material/Button";
+import { CustomButtonProps } from "./CustomButton.types";
+
+const CustomButton: React.FC<CustomButtonProps> = ({
+  buttonTitle,
+  type = "button",
+  variant = "contained",
+  color = "primary",
+}) => {
+  return (
+    <Button color={color} type={type} variant={variant}>
+      {buttonTitle}
+    </Button>
+  );
+};
+
+export default CustomButton;
---
+++ UITechTest/src/Components/Button/CustomButton.types.ts
@@ -0,0 +1,17 @@
+type ButtonType = "submit" | "reset" | "button";
+type buttonVariant = "contained" | "outlined" | "text";
+type buttonColor =
+  | "error"
+  | "info"
+  | "inherit"
+  | "primary"
+  | "secondary"
+  | "success"
+  | "warning";
+
+export interface CustomButtonProps {
+  buttonTitle: string;
+  type?: ButtonType;
+  variant?: buttonVariant;
+  color?: buttonColor;
+}
